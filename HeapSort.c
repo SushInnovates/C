e7dd9056f51a02c0ed05b49028e90a51640120f4
Sushant Sutar <sushant.sutR@gmail.com>
2025-10-16 21:09:02 +0530

    Update HeapSort.c

--- HeapSort.c
+++ HeapSort.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
-// Function to swap two elements
+
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-// Heapify a subtree rooted at index i
+
 void heapify(int arr[], int n, int i) {
     int largest = i;          // Initialize largest as root
     int left = 2 * i + 1;     // left child
     int right = 2 * i + 2;    // right child
 
-    // If left child is larger than root
+  
     if (left < n && arr[left] > arr[largest])
         largest = left;
 
-    // If right child is larger than largest so far
+
     if (right < n && arr[right] > arr[largest])
         largest = right;
 
-    // If largest is not root
+
     if (largest != i) {
         swap(&arr[i], &arr[largest]);
 
-        // Recursively heapify the affected subtree
+       
         heapify(arr, n, largest);
     }
 }
